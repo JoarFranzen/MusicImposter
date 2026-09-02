ebab4fbf3f64d7c8d241cb91aad41c9e54bebecf
JoarFranzen <joarfranzen@gmail.com>
2026-09-02 09:53:48 +0200

    Add Vite + React scaffolding for Cloudflare Pages build

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect, useRef } from "react";
+
+const THEMES = [
+  "Heavy Metal", "Smooth Jazz", "Classical Piano", "Country Ballad", "Gospel Choir",
+  "Death Metal", "Bossa Nova", "Bluegrass", "K-Pop", "Afrobeats",
+  "Reggaeton", "Drill Rap", "Lo-Fi Hip Hop", "Indie Folk", "Synthwave",
+  "Dancehall", "Deep House", "Punk Rock", "Ambient Techno", "Flamenco",
+  "Delta Blues", "Trap Music", "Barbershop Quartet", "Doo-Wop", "Neo Soul"
+];
+// PLACEHOLDER_FULL_CONTENT
